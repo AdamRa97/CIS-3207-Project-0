bf752d4f19a0de1fafd44b42799a4007461e7237
Adam Ra <adamra@Adams-MacBook-Pro-2.local>
2023-02-06 16:21:53 -0500

    'tucp' can now copy files, still need to find out how to copy multiple at the same time and how to not manually set buffer size

--- tucp.c
+++ tucp.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define BUF_SIZE 1024
 
 int main(int argc, char *argv[]){
-    return 0;
+    
+    FILE *src, *dst;
+    size_t in, out;
+    char buf[BUF_SIZE];
+    int bufsize;
 
+    bufsize = atoi(argv[1]);
+    printf("bufsize = %d",bufsize);
 
+    src = fopen(argv[2], "r");
+
+    if (NULL == src) exit(2);
+        dst = fopen(argv[3], "w");
+    if (dst < 0)
+        exit(3);
+
+    while (1){
+        in = fread(buf, 1, bufsize, src);
+        if (0 == in)
+            break;
+        out = fwrite(buf, 1, in, dst);
+        if (0 == out)
+            break;
+    }
+
+    fclose(src);
+    fclose(dst);
+    exit(0);
 }

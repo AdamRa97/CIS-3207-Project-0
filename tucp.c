e97598caf01bd7f3fba8632e5e6fedfbad81517b
Adam Ra <adamra@Adams-MacBook-Pro-2.local>
2023-02-07 10:06:14 -0500

    Found a way to always target the last argument which will always be the dest, still need to find a way to prevent segmentation faults

--- tucp.c
+++ tucp.c
@@ -1,8 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+#define BUF_SIZE 1024
 
 int main(int argc, char *argv[]){
-    return 0;
+    
+    // FILE *src, *dst;
+    // size_t in, out;
+    // char buf[BUF_SIZE];
+    
+    printf("size of argv[] : %d\n", argc);
+    printf("arg[-1] : %s", argv[argc-1]);
 
 
+    // for (int i = 1; i < argc; i++){
+        
+    //     src = fopen(argv[i], "r");
+
+    //     if (src == NULL)
+    //         exit(2);
+
+    //     dst = fopen(argv[argc-1], "w");
+
+    //     if (dst < 0)
+    //         exit(3);
+
+    //     while (1){
+    //         in = fread(buf, 1, 1, src);
+    //         if (0 == in)
+    //             break;
+    //         out = fwrite(buf, 1, in, dst);
+    //         if (0 == out)
+    //             break;
+    //     }
+    // }
+    // src = fopen(argv[1], "r");
+
+    // if (NULL == src) exit(2);
+    //     dst = fopen(argv[2], "w");
+    // if (dst < 0)
+    //     exit(3);
+
+    // while (1){
+    //     in = fread(buf, 1, 1, src);
+    //     if (0 == in)
+    //         break;
+    //     out = fwrite(buf, 1, in, dst);
+    //     if (0 == out)
+    //         break;
+        // }
+    // fclose(src);
+    // fclose(dst);
+    // exit(0);
 }

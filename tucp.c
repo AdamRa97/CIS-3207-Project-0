70f0dc09bdc73c40ed27c7b5182cd4e666632f5c
Adam Ra <adamra@3c-06-30-16-93-bf.wireless.temple.edu>
2023-02-07 13:58:51 -0500

    Figured out how to copy files when specifying what new file to name it, need to find out how to make it move solely on directory now

--- tucp.c
+++ tucp.c
@@ -8,21 +8,15 @@ int main(int argc, char *argv[]){
     FILE *src, *dst;
     size_t in, out;
     char buf[BUF_SIZE];
-    int bufsize;
+    int bufsize = 1024;
     int count = 0;
     
     printf("size of argv[] : %d\n", argc);
     printf("arg[-1] : %s\n", argv[argc-1]);
 
-
     for (int i = 1; i < argc-1; i++){
-        bufsize = atoi(argv[i]);
 
-        if (bufsize > BUF_SIZE) {
-            fprintf(stderr,"Error: %s: max. buffer size is %d\n",argv[i], BUF_SIZE);
-            exit(1);
-        }
-
+        printf("source opening %s\n",argv[i]);
         src = fopen(argv[i], "r");
         if (src == NULL){
             printf("the src is null");
@@ -31,10 +25,11 @@ int main(int argc, char *argv[]){
         printf("arg[%d]: %s\n", i,argv[i]);
 
         dst = fopen(argv[argc-1], "w");
-        printf("arg[%d]: %s\n", argc-1,argv[argc-1]);
+        printf("dst: %s\n",argv[argc-1]);
 
         if (dst < 0){
             printf("dst is less than 0");
+            // exit is placeholder for now, need to make directory instead later on
             exit(3);
         }
 
@@ -43,6 +38,7 @@ int main(int argc, char *argv[]){
             printf("count = %d\n",count);
             printf("this is being called from the While Loop\n");
             in = fread(buf, 1, bufsize, src);
+            // printf("in == %zu\n", in);
             if (0 == in){
                 printf("in == 0");
                 break;

9b6f04af5b630e22977eb9156c51db91da3ef152
Adam Ra <adamra@3c-06-30-16-93-bf.wireless.temple.edu>
2023-02-07 11:51:47 -0500

    Added printf's for debugging

--- tucp.c
+++ tucp.c
@@ -9,6 +9,7 @@ int main(int argc, char *argv[]){
     size_t in, out;
     char buf[BUF_SIZE];
     int bufsize;
+    int count = 0;
     
     printf("size of argv[] : %d\n", argc);
     printf("arg[-1] : %s\n", argv[argc-1]);
@@ -23,26 +24,34 @@ int main(int argc, char *argv[]){
         }
 
         src = fopen(argv[i], "r");
-
-        if (src == NULL)
+        if (src == NULL){
+            printf("the src is null");
             exit(2);
-        // printf("arg[%d]: %s\n", i,argv[i]);
+        }
+        printf("arg[%d]: %s\n", i,argv[i]);
 
         dst = fopen(argv[argc-1], "w");
-        // printf("arg[%d]: %s\n", argc-1,argv[argc-1]);
+        printf("arg[%d]: %s\n", argc-1,argv[argc-1]);
 
-        if (dst < 0)
-            printf("this is being called");
+        if (dst < 0){
+            printf("dst is less than 0");
             exit(3);
+        }
 
         while (1){
-            printf("this is being called from the While Loop");
+            count++;
+            printf("count = %d\n",count);
+            printf("this is being called from the While Loop\n");
             in = fread(buf, 1, bufsize, src);
-            if (0 == in)
+            if (0 == in){
+                printf("in == 0");
                 break;
+            }
             out = fwrite(buf, 1, in, dst);
-            if (0 == out)
+            if (0 == out){
+                printf("out == 0");
                 break;
+            }
         }
 
         fclose(src);

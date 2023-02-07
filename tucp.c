6b4ff94b854c72e6bd4371fc5b1de64d4311eb30
Adam Ra <adamra@Adams-MacBook-Pro-2.local>
2023-02-07 10:29:09 -0500

    Found a way to iterate through each argument, and able to open through the files, need to find a way to copy the contents through the while loop

--- tucp.c
+++ tucp.c
@@ -5,51 +5,48 @@
 
 int main(int argc, char *argv[]){
     
-    // FILE *src, *dst;
-    // size_t in, out;
-    // char buf[BUF_SIZE];
+    FILE *src, *dst;
+    size_t in, out;
+    char buf[BUF_SIZE];
+    int bufsize;
     
     printf("size of argv[] : %d\n", argc);
-    printf("arg[-1] : %s", argv[argc-1]);
+    printf("arg[-1] : %s\n", argv[argc-1]);
 
 
-    // for (int i = 1; i < argc; i++){
-        
-    //     src = fopen(argv[i], "r");
+    for (int i = 1; i < argc-1; i++){
+        bufsize = atoi(argv[i]);
 
-    //     if (src == NULL)
-    //         exit(2);
+        if (bufsize > BUF_SIZE) {
+            fprintf(stderr,"Error: %s: max. buffer size is %d\n",argv[i], BUF_SIZE);
+            exit(1);
+        }
 
-    //     dst = fopen(argv[argc-1], "w");
+        src = fopen(argv[i], "r");
 
-    //     if (dst < 0)
-    //         exit(3);
+        if (src == NULL)
+            exit(2);
+        // printf("arg[%d]: %s\n", i,argv[i]);
 
-    //     while (1){
-    //         in = fread(buf, 1, 1, src);
-    //         if (0 == in)
-    //             break;
-    //         out = fwrite(buf, 1, in, dst);
-    //         if (0 == out)
-    //             break;
-    //     }
-    // }
-    // src = fopen(argv[1], "r");
+        dst = fopen(argv[argc-1], "w");
+        // printf("arg[%d]: %s\n", argc-1,argv[argc-1]);
 
-    // if (NULL == src) exit(2);
-    //     dst = fopen(argv[2], "w");
-    // if (dst < 0)
-    //     exit(3);
+        if (dst < 0)
+            printf("this is being called");
+            exit(3);
 
-    // while (1){
-    //     in = fread(buf, 1, 1, src);
-    //     if (0 == in)
-    //         break;
-    //     out = fwrite(buf, 1, in, dst);
-    //     if (0 == out)
-    //         break;
-        // }
-    // fclose(src);
-    // fclose(dst);
-    // exit(0);
+        while (1){
+            printf("this is being called from the While Loop");
+            in = fread(buf, 1, bufsize, src);
+            if (0 == in)
+                break;
+            out = fwrite(buf, 1, in, dst);
+            if (0 == out)
+                break;
+        }
+
+        fclose(src);
+        fclose(dst);
+    }
+    exit(0);
 }
